53c6772ad739648f86c5c538d1d5dff2d8d57167
sonhak-18 <100685253+sonhak-18@users.noreply.github.com>
2024-07-09 12:01:30 +0700

    several ui adjustment

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,10 +58,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Use ask to: @Yona_io'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+                       'Use ask to: @Yona_io to use me.\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
