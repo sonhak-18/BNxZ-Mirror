bba5cbd3a5d5d2b434c5309dd98d752edbcfef30
sonhak-18 <100685253+sonhak-18@users.noreply.github.com>
2024-07-10 15:34:07 +0700

    Rework index urls

--- bot/helper/mirror_utils/gdrive_utils/search.py
+++ bot/helper/mirror_utils/gdrive_utils/search.py
@@ -126,9 +126,14 @@ class gdSearch(GoogleDriveHelper):
                     furl = f"https://drive.google.com/drive/folders/{file.get('id')}"
                     msg += f"🗂 <b>{file.get('name')}<br>(folder)</b><br>"
                     if not config_dict['DISABLE_DRIVE_LINK']:
-                        msg += f"<b><a href={furl}>Drive Link</a></b> | "
+                        msg += f"<b><a href={furl}>☁️ Drive Link</a></b> | "
                     if index_url:
-                        url = f'{index_url}findpath?id={file.get("id")}'
+                        if isRecur:
+                            url_path = "/".join(rquote(n, safe='')
+                                                for n in self.__get_recursive_list(file, dir_id))
+                        else:
+                            url_path = rquote(f'{file.get("name")}', safe='')
+                        url = f'{index_url}/{url_path}/'
                         msg += f'<b><a href="{url}">⚡️ Index Link</a></b>'
                 elif mime_type == 'application/vnd.google-apps.shortcut':
                     furl = f"https://drive.google.com/drive/folders/{file.get('id')}"
@@ -140,10 +145,15 @@ class gdSearch(GoogleDriveHelper):
                     if not config_dict['DISABLE_DRIVE_LINK']:
                         msg += f"<b><a href={furl}>☁️ Drive Link</a></b> | "
                     if index_url:
-                        url = f'{index_url}findpath?id={file.get("id")}'
+                        if isRecur:
+                            url_path = "/".join(rquote(n, safe='')
+                                                for n in self.__get_recursive_list(file, dir_id))
+                        else:
+                            url_path = rquote(f'{file.get("name")}', safe='')
+                        url = f'{index_url}/{url_path}/'
                         msg += f'<b><a href="{url}">⚡️ Index Link</a></b>'
                         if mime_type.startswith(('image', 'video', 'audio')):
-                            urlv = f'{index_url}findpath?id={file.get("id")}&view=true'
+                            urlv = f'{index_url}/{url_path}&view=true'
                             msg += f' | <b><a href="{urlv}">View Link</a></b>'
                 msg += '<br><br>'
                 contents_no += 1
